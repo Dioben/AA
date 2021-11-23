556fc6cc28617cc321bd3a1956eb6b00b80b4955
buckaroo69 <57142107+buckaroo69@users.noreply.github.com>
2021-11-23 22:38:46 +0000

    avoid massive runtime operations

--- assignment1/datagen.sh
+++ assignment1/datagen.sh
@@ -7,8 +7,11 @@ minnodes=$((nodes-1))
 maxnodes=$((nodes*(nodes-1)/2))
 for edges in $(seq $minnodes $maxnodes)
 do
+if [ $edges -le 20 ] 
+then
 python3 graph.py --nodes $nodes --edges $edges --seed 93391
 echo "${nodes},${edges},exhaustive,$(python3 solver.py --exhaustive --no-graph)" >>results.csv
 echo "${nodes},${edges},greedy,$(python3 solver.py --no-graph)" >>results.csv
+fi
 done
 done

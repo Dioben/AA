fe9238881110a5a1be52a1193f897ae3fdf60f18
buckaroo69 <diogobento@ua.pt>
2021-12-14 12:57:15 +0000

    class checkpoint

--- assignment2/counters.py
+++ assignment2/counters.py
@@ -1,4 +1,5 @@
 import random
+from math import sqrt
 
 def normalCounter(text):
     results = {}
@@ -31,13 +32,18 @@ def dynamicProbabilityCounter(text,probf,memorized = []):
                 results[char]+=1
     return results
 
+
+def invSqrtFunc(value):
+    return 1/(sqrt(2)**value)
+
 if __name__ == "__main__":
     filepaths = ["english.txt","french.txt","german.txt"]
+    table = [invSqrtFunc(x) for x in range(0,20001)] #pre calculate character 
     for filepath in filepaths:
         f = open(filepath,"r")
         text = f.read()
         f.close()
         accurateCount = normalCounter(text)
         staticProbCount = staticProbabilityCounter(text,1/16)
-        dynamicProbCount = dynamicProbabilityCounter(text,sqrtfunc,table)
-        
+        dynamicProbCount = dynamicProbabilityCounter(text,invSqrtFunc,table)
+

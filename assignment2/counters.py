bdf3a89aa1329d9933c2d6e74fc2d2ebf4b205a0
buckaroo69 <diogobento@ua.pt>
2021-12-17 12:00:43 +0000

    just need to parse results now

--- assignment2/counters.py
+++ assignment2/counters.py
@@ -1,5 +1,7 @@
+import argparse
 import random
 from math import sqrt
+import json
 
 def normalCounter(text):
     results = {}
@@ -24,18 +26,21 @@ def staticProbabilityCounter(text,prob):
 def dynamicProbabilityCounter(text,probf,memorized = []):
     results = {}
     for char in text:
+        
         if char not in results:
             value = 0
         else:
             value = results[char]
-            roll = random.random()
+        
+        roll = random.random()
         try:
             limit = memorized[value]
             if roll<=limit:
                 results[char] = value+1
         except:
             #we have gone past the memorized maximum
-            memorized+= [probf(x) for x in range(101)] #calculate the next 100 entries
+            memcount = len(memorized)
+            memorized+= [probf(x) for x in range(memcount,memcount+21)] #calculate the next 20 entries
             limit = memorized[value]
             if roll<=limit:
                 results[char] = value+1
@@ -46,13 +51,31 @@ def invSqrtFunc(value):
     return 1/(sqrt(2)**value)
 
 if __name__ == "__main__":
-    filepaths = ["english.txt","french.txt","german.txt"]
-    table = [invSqrtFunc(x) for x in range(101)] #pre calculate character 
+    parser= argparse.ArgumentParser()
+    parser.add_argument("--files",help="comma separated input files", default="english.txt,french.txt,german.txt")
+    parser.add_argument("--count",help="how many times to run each counter",type=int, default=1000)
+    parser.add_argument("--output",help="file to dump information into")
+    args = parser.parse_args()
+    filepaths = args.files.split(",")
+    table = [invSqrtFunc(x) for x in range(101)] #pre calculate character
+
+    results = {}
     for filepath in filepaths:
         f = open(filepath,"r")
         text = f.read()
         f.close()
         accurateCount = normalCounter(text)
-        staticProbCount = staticProbabilityCounter(text,1/16)
-        dynamicProbCount = dynamicProbabilityCounter(text,invSqrtFunc,table)
+        staticProbCount = []
+        dynamicProbCount = []
+        for _ in range(args.count):
+            staticProbCount.append( staticProbabilityCounter(text,1/16))
+            dynamicProbCount.append(dynamicProbabilityCounter(text,invSqrtFunc,table))
+        results[filepath] = {"real":accurateCount,"static":staticProbCount,"dynamic":dynamicProbCount}
+
+    if args.output:
+        f = open(args.output,"w")
+        json.dump(results,f)
+        f.close()
+    else:
+        print(results)  
 

fd824a3a946a0f7e89b3816330186857e1c4858d
buckaroo69 <diogobento@ua.pt>
2021-12-19 13:07:01 +0000

    analysis bug fixed

--- assignment2/analysis.py
+++ assignment2/analysis.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+from math import inf
 
 def upscaleValues(data,fixedMult,dynamicMod,memorized = []):
     for idx in range(len(data['static'])):
@@ -17,6 +18,65 @@ def upscaleValues(data,fixedMult,dynamicMod,memorized = []):
     return data
 
 
+def getStats(data):
+    results = {"static":{},"dynamic":{}}
+    sizes = [1,10,25,50,100,175,250,375,500,750,1000]
+    real = data['real']
+    for size in sizes:
+        staticList = data['static'][:size]
+        dynamicList = data['dynamic'][:size]
+        max,min = getDiffs(real,staticList)
+        dyn_max,dyn_min = getDiffs(real,dynamicList)
+
+        avgStatic = {x:sum([item.get(x,0) for item in staticList])/size for x in real.keys()}
+        avgDynamic = {x:sum([item.get(x,0) for item in dynamicList])/size for x in real.keys()}
+        
+        avgErrorStatic = sum( [abs(y-avgStatic[x]) for x,y in real.items()] ) /len(real.keys()) 
+        avgErrorDynamic = sum( [abs(y-avgDynamic[x]) for x,y in real.items()] ) /len(real.keys())
+
+        realSorted = sorted(real.keys(),key=lambda x:real[x])
+        staticSorted = sorted(avgStatic.keys(),key=lambda x:avgStatic[x])
+        dynamicSorted = sorted(avgDynamic.keys(),key=lambda x:avgDynamic[x])
+        
+        staticSwaps = countSwaps(realSorted,staticSorted)
+        dynamicSwaps = countSwaps(realSorted,dynamicSorted)
+        
+        results[size] = {"max":max,"min":min,"dyn_max":dyn_max,"dyn_min":dyn_min,
+                        "avgdiff":avgErrorStatic,"dyn_avgdiff":avgErrorDynamic,"swaps":staticSwaps,"dyn_swaps":dynamicSwaps}
+    return results
+
+
+def getDiffs(real,list):
+    max= 0
+    min = inf
+    for letter,value in real.items():
+        for datapoint in list:
+            value2 = datapoint.get(letter,0)
+            delta = abs(value-value2)
+            if max<delta:
+                max = delta
+            if min>delta:
+                min = delta
+    return max,min
+
+def countSwaps(realSorted,otherSorted):
+    swaps = 0
+    for index in range(len(realSorted)):
+            realChar = realSorted[index]
+            otherChar = otherSorted[index]
+          
+            if otherChar!=realChar:
+                swaps+=1
+                otheridx = otherSorted.index(realChar)
+                otherSorted[otheridx]= otherChar
+                otherSorted[index]=realChar
+
+    assert realSorted == otherSorted
+    return swaps
+
+
+
+
 def inverseDynamicMod(value):
     #my factor is 1/(sqrt(2)**k)
     #equivalent to 1/(2 **1/2 **k) = 1/(2 **k/2)
@@ -32,6 +92,12 @@ if __name__ == "__main__":
     f = open(args.source,"r")
     data = json.load(f)
     f.close()
-    for info in data.values():
-        upscaleValues(info,16,inverseDynamicMod,[inverseDynamicMod(x) for x in range(101)])
+    results = {}
+    for name,info in data.items():
+        info = upscaleValues(info,16,inverseDynamicMod,[inverseDynamicMod(x) for x in range(101)])
+        results[name] = getStats(info)
+    
+    for x,y in results.items():
+        print(x)
+        print(y)
         

a29f1f5c47970c49883e3055e38d69dd69a74e53
buckaroo69 <diogobento@ua.pt>
2021-12-20 15:16:06 +0000

    converter has 2 params now

--- assignment2/converter.py
+++ assignment2/converter.py
@@ -1,13 +1,13 @@
-def convertFile(filename):
+def convertFile(filename,output):
     f = open(filename,"r")
     contents = f.read()
     contents = [x.upper() for x in contents if x.isalpha()]
     f.close()
-    f = open(filename,"w")
+    f = open(output,"w")
     f.write("".join(contents))
     f.close()
 
 if __name__=="__main__":
-    convertFile("english.txt")
-    convertFile("french.txt")
-    convertFile("german.txt")
+    convertFile("englishText.txt","english.txt")
+    convertFile("frenchText.txt","french.txt")
+    convertFile("germanText.txt","german.txt")

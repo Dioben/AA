89b5ca5433027c09fd6f83e38011a11bce95f0af
buckaroo69 <diogobento@ua.pt>
2022-02-03 23:25:39 +0000

    new take on cms

---
+++ assignment3/altcounters.py
@@ -0,0 +1,47 @@
+import argparse
+import json
+from collections import Counter
+from newMinSketch import CountMinSketch
+from heapq import heapify,heappushpop
+
+
+if __name__ == "__main__":
+    parser= argparse.ArgumentParser()
+    parser.add_argument("--files",help="comma separated input files", default="english.txt,french.txt,german.txt")
+    parser.add_argument("--output",help="file to dump information into")
+    args = parser.parse_args()
+    filepaths = args.files.split(",")
+   
+    results = {fp.removesuffix(".txt"):{"sketch":[],"snapshot":[],"real":{}} for fp in filepaths}
+    for filepath in filepaths:
+        keyname = filepath.removesuffix(".txt")
+        with  open(filepath,"r") as f:
+            words = f.read().split(" ")
+        real = Counter(words)
+        for hashes in range(3,11):
+            for hashsize in range (1,21):
+                sketch = CountMinSketch(d=hashes, m= int(len(real.keys()) *hashsize/10) )
+                heap = [(0,"") for _ in range(100)]
+                heapify(heap)
+                for word in words:
+                    sketch.update(word)
+                    val = sketch.query(word)
+                    wordseen = [i for i,s in enumerate(heap) if s[1] == word] #only supposed to have one element at most
+                    if wordseen:
+                        heap[wordseen[0]] = (val,word)
+                        heapify(heap)
+                    else:    
+                        heappushpop(heap,(val,word)) #attempt to add this to heap
+
+                results[keyname]['snapshot'].append({"hashes":hashes,"size":hashsize/10, "values":[y for x,y in sorted(heap, reverse=True, key=lambda x: x[0]) ] }) #top 100 sketch values when the words actually appeared (therefore equal or smaller)
+                results[keyname]['sketch'].append({"hashes":hashes,"size":hashsize/10,"values": {x:int(sketch.query(x)) for x in real.keys()} } ) #converting is necessary because of JSON 
+        results[keyname]['real'] = real
+        
+        
+    if args.output:
+        f = open(args.output,"w")
+        json.dump(results,f)
+        f.close()
+    else:
+        print(results)  
+
---
+++ assignment3/newMinSketch.py
@@ -0,0 +1,115 @@
+# Rafael Carrascosa's Count-Min Sketch pure python implementation
+#
+# Adapted from https://github.com/rafacarrascosa/countminsketch
+#
+# Using delta and epsilon as suggested in https://github.com/AWNystrom/CountMinSketch
+#
+# J. Madeira --- December 2018
+#
+# Further Adjustments by Diogo Bento --- January 2021
+
+import math
+
+import numpy as np
+
+class CountMinSketch(object):
+    """
+    A class for counting hashable items using the Count-min Sketch strategy.
+
+    The Count-min Sketch is a randomized data structure that uses a constant
+    amount of memory and has constant insertion and lookup times at the cost
+    of an arbitrarily small overestimation of the counts.
+
+    It has two parameters:
+     - `m` the size of the hash tables, larger implies smaller overestimation
+     - `d` the number of hash tables, larger implies lower probability of
+           overestimation.
+
+    Note that this class can be used to count *any* hashable type, so it's
+    possible to "count apples" and then "ask for oranges". Validation is up to
+    the user.
+    """
+
+    def __init__(self, m=None, d=None, delta=None, epsilon=None):
+        """
+        Parameters
+        ----------
+        m : the number of columns in the count matrix
+        d : the number of rows in the count matrix -> formal language has |rows| hash functions, even if it seems opposite of what's intuitive
+        delta : (not applicable if m and d are supplied) the probability of query error
+        epsilon : (not applicable if w and d are supplied) the query error factor
+        """
+        
+        #CHANGELOG:
+        """
+        print statement has been moved to __str__
+        table is now numpy based
+        """
+        if m is not None and d is not None:
+            self.m = m
+            self.d = d
+        elif delta is not None and epsilon is not None:
+            # Computing the size of the sketch
+            self.m = math.ceil(2.0 / epsilon)
+            self.d = math.ceil(math.log(1.0 / delta))
+        else:
+            raise ValueError( "You must either supply both m and d or delta and epsilon.")
+        
+
+        self.n = 0
+
+        self.table = np.zeros((d,m),dtype=np.uint64)
+
+    def _hash(self,x):
+        #CHANGELOG: Entirely redone for better readability
+        values = []
+        for i in range(self.d):
+            item = (x,i)
+            values.append( abs(hash(item)) % self.m)
+        return values
+
+    def update(self, x):
+        """
+        Count element `x` as if had appeared `value` times.
+        By default `value=1` so:
+
+            sketch.add(x)
+
+        Effectively counts `x` as occurring once.
+        """
+        #CHANGELOG:
+        '''
+        removed the value parameter
+        changed indexing a bit
+        now only increments buckets matching smallest value
+        '''
+        self.n += 1
+        indexes = [x for x in zip( range(self.d) , self._hash(x))]
+        minval = min([ self.table[index] for index in indexes])
+        indexes = [index for index in indexes if self.table[index]==minval]
+        for index in indexes:
+            self.table[index] += 1
+
+    def query(self, x):
+        """
+        Return an estimation of the amount of times `x` has ocurred.
+        The returned value always overestimates the real value.
+        """
+        #CHANGELOG: Changed indexing a bit
+        return min(self.table[index] for index in zip(range(self.d), self._hash(x)))
+
+    def __getitem__(self, x):
+        """
+        A convenience method to call `query`.
+        """
+        return self.query(x)
+
+    def __len__(self):
+        """
+        The number of things counted. Takes into account that the `value`
+        argument of `add` might be different from 1.
+        """
+        return self.n
+
+    def __str__(self):
+            return("CM Sketch with " + str(self.m) + " columns and " + str(self.d) + " rows")
